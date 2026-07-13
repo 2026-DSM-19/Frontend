bf984e802aef5160047d85c68e3765d6d763b4e7
jaejun <ssokage090210@naver.com>
2026-07-14 08:51:52 +0900

    레이아웃 구성
    레이아웃 구성

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Layout from "./layout";
+
+function App() {
+  return (
+    <Layout>
+      <div>
+        <h1>Welcome to the App!</h1>
+        <p>This is the main content of the application.</p>
+      </div>
+    </Layout>
+  );
+}
+
+export default App;
